d38a1e60e1a1b10dafe2202476941234a483e204
Eliott Vincent <dev@eliottvincent.com>
2022-12-22 15:54:32 +0100

    Add set method

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ Object.defineProperty(exports, "__esModule", {
  * @return {string|number|boolean|object} The value within the object
  */
 var get = function(object, path = "", defaultValue = undefined) {
-  let _value = object,
+  var _value = object,
       _path  = path.split(".");
 
-  for (let _i = 0; _i < _path.length; _i++) {
+  for (var _i = 0; _i < _path.length; _i++) {
     if (!_value) {
       break;
     }
@@ -31,7 +31,36 @@ var get = function(object, path = "", defaultValue = undefined) {
   return _value;
 };
 
+/**
+ * Sets a value at a path within an object
+ * @public
+ * @param  {object}                       object
+ * @param  {string}                       path
+ * @param  {string|number|boolean|object} value
+ * @return {undefined}
+ */
+var set = function(object, path, value) {
+  var _path = path.split(".");
+
+  for (var _i = 0; _i < _path.length; _i++) {
+    // Reached the end?
+    if (_i === _path.length - 1) {
+      object[_path[_i]] = value;
+    } else {
+      // Initiate the path?
+      if (!object[_path[_i]]) {
+        object[_path[_i]] = {};
+      }
+
+      // Continue the path
+      object = object[_path[_i]];
+    }
+  }
+};
+
 exports.get     = get;
+exports.set     = set;
 exports.default = {
   get    : get,
+  set    : set,
 };
